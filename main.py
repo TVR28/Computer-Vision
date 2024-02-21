31018656d46c8d86bca5ae16958940052a3c1308
Raviteja Tanikella <91713140+TVR28@users.noreply.github.com>
2024-02-21 15:32:15 -0800

    Updated with object detection code
    Updated Object Detection code and outputs

---
+++ main.py
@@ -0,0 +1,29 @@
+from ultralytics import YOLO
+import cv2
+
+
+# load yolov8 model
+model = YOLO('yolov8n.pt')
+
+# load video
+video_path = './test1.mp4'
+cap = cv2.VideoCapture(video_path) 
+
+ret = True
+# read frames
+while ret:
+    ret, frame = cap.read() #returns new frame from video
+
+    if ret:
+
+        # detect objects
+        # track objects
+        results = model.track(frame, persist=True)
+
+        # plot results
+        frame_ = results[0].plot()
+
+        # visualize
+        cv2.imshow('frame', frame_)
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
